eb499a587c32186d617963903a5297cde0a2d2a2
FaridaBarkenkhoyeva <barkenkhoyeva.farida@outlook.com>
2025-06-07 15:38:29 +0200

    moving states and functions to context file

--- src/ViewEntryModal.jsx
+++ src/ViewEntryModal.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import EntryCard from "./EntryCard";
+import { useAppContext } from "./context/appContext";
 
-export default function ViewEntryModal({ entry }) {
+export default function ViewEntryModal() {
+  const { selectedEntry } = useAppContext();
+
   return (
     <>
       {/* Open the modal using document.getElementById('ID').showModal() method */}
@@ -14,7 +16,48 @@ export default function ViewEntryModal({ entry }) {
       <dialog id="my_modal_6" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box w-full max-width-4-xl max-h-[90vh] overflow-y-auto">
           <div className="modal-action">
-            {entry && <EntryCard entry={entry} /> }
+            {selectedEntry && (
+              <>
+                <div className="card bg-base-100 w-full md:w-90% lg:w-90% xl:w-90% shadow-sm border-amber-100 mx-auto">
+                  <div className="card-body items-center text-center">
+                    <h2 className="card-title bg-stone-100 border-amber-100 rounded-md px-4 py-1 text-sm md:text-base lg:text-lg">
+                      {selectedEntry?.title}
+                    </h2>
+                  </div>
+
+                  <div className="flex gap-4 m-4 items-stretch">
+                    <div className="flex flex-col w-full ">
+                      <figure className="flex-1">
+                        <img
+                          src={selectedEntry?.imagePreviewUrl}
+                          alt="Shoes"
+                          className=" h-full w-full rounded-xl object-contain"
+                        />
+                      </figure>
+
+                      <div className="card-actions m-2 justify-start">
+                        <div className=" w-1/3 flex gap-2">
+                          <button className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+                            Update
+                          </button>
+                          <button className="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="w-full p-2 ">
+                      <p className="text-xs md:text-sm lg:text-base">
+                        {" "}
+                        {selectedEntry?.fullText}
+                      </p>
+                      <p>{selectedEntry?.date}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
             {/* <div>
               {entry && (
                 <EntryCard
